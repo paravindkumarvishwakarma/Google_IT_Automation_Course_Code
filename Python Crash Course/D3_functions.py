9ecf52ec46880762f5d9dee8af89a46fd4ea19ff
ParavindVishwakarma1 <pravindkumarvishwakarma@gmail.com>
2024-05-11 00:05:38 +0530

    Defining functions

---
+++ Python Crash Course/D3_functions.py
@@ -0,0 +1,13 @@
+def greeting(name):
+    print("Welcome, " + name)
+    
+greeting("Suraj")
+greeting("Pwana")
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+    
+greeting("Paravind", "Software engineering")
+greeting("Suraj", "Software engineering")
+greeting("Pawan", "Software engineering")
