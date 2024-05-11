c946af58166091f494ee54e4d539825a45119f0d
ParavindVishwakarma1 <pravindkumarvishwakarma@gmail.com>
2024-05-11 19:55:52 +0530

    Practice Quize

--- Python Crash Course/D3_functions.py
+++ Python Crash Course/D3_functions.py
@@ -40,4 +40,86 @@ def greating(name):
 result = greating("Paravind")
 print(result)
 
+name = "paravind" 
+number = len(name) * 9
+print("Hello" + name + ". Your lucky number is " + str(number))
+
+name = "Pawan"
+number = len(name) * 9
+print("Hello" + name + ". Your lucky number is " + str(number))
+
+#We can write above code in simple way using functions
+def lucky_number(name): #Find the lucky number
+    number = len(name) * 9
+    print("Hello" + name + ". Your lucky number is " + str(number))
+
+lucky_number("Paravind Kumar Vishwakarma")
+lucky_number("Pawan Chaturvedi")
+
+def calculate(d):
+    q = 3.14
+    z = q * (d**2)
+    print(z)
+
+calculate(5) #Output is 78.5
+
+def circle_area(radius): #Find the area of circle
+    pi = 3.14
+    area = pi * (radius ** 2)
+    print(area)
+
+circle_area(5) #Outpur is 78.5
+
+"""Skill Group 1
+
+    Use a function that accepts multiple parameters
+
+    Return a result value"""
+
+# This function calculates the number of days in a variable number of 
+# years, months, and days. These variables are provided by the user and
+# are passed to the function through the function’s parameters.
+def find_total_days(years, months, days):
+# Assign a variable to hold the calculations for the number of days in
+# a year (years*365) plus the number of days in a month (months*30) plus
+# the number of days provided through the "days" parameter variable.
+    my_days = (years*365) + (months*30) + days
+# Use the "return" keyword to send the result of the "my_days"  
+# calculation to the function call. 
+    return my_days
+ 
+# Function call with user provided parameter values. 
+print(find_total_days(2,5,23))
+
+"""Skill Group 2
+
+    Use a function to return the result of a measurement conversion
+
+    Use arithmetic operators to perform a calculation
+
+    Combine text with a function call within a print() statement
+
+    Convert the return value from a float data type to a string for the print() function
+
+    Call the function and perform a calculation on the return value within a print() statement"""
+
+# This function converts fluid ounces to milliliters and returns the 
+# result of the conversion.
+def convert_volume(fluid_ounce):
+# Calculate value of the "ml" variable using the parameter variable 
+# "fluid_ounce". There are approximately 29.5 milliliters in 1 fluid
+# ounce.
+    ml = fluid_ounce * 29.5  
+# Return the result of the calculation.  
+    return ml
+ 
+# Call the conversion from within the print() function using 2 fluid 
+# ounces. Convert the return value from a float to a string.  
+print("The volume in milliliters is " + str(convert_volume(2)))
+ 
+# Call the function again and double the 2 fluid ounces from within
+# the print function.
+print("The volume in milliliters is " + str(convert_volume(2)*2))
+# Alternative calculation:
+# print("The volume in milliliters is " + str(convert_volume(4))
 
--- Python Crash Course/D4_Built_in_functions.py
+++ Python Crash Course/D4_Built_in_functions.py
@@ -24,3 +24,4 @@ print("max() and min()")
 time_list = [12,2,32,19,57,22,14]
 print(min(time_list))
 print(max(time_list))
+
