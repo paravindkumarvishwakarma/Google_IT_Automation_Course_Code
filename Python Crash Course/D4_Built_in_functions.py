ac4759a35121cbbdb7177edcc8c6362b82f9c7c5
ParavindVishwakarma1 <pravindkumarvishwakarma@gmail.com>
2024-05-11 18:19:35 +0530

    built in functions

---
+++ Python Crash Course/D4_Built_in_functions.py
@@ -0,0 +1,26 @@
+#print() functions
+print("print() functions")
+month = "September"
+print("Investigate failed login attemps during", month, "if more than", 100)
+
+#type(f) functions
+print("type() functions")
+print(type("This is a string"))
+
+#str() functions
+print("str() functions")
+number = 12
+string_representation = str(number)
+print(string_representation)
+
+#sorted() functions
+print("sorted() functions")
+time_list = [12,2,32,19,57,22,14]
+print(sorted(time_list))
+print(time_list) #The sorted() functions does nit change the iterable that it sorts.
+
+#max() and min()
+print("max() and min()")
+time_list = [12,2,32,19,57,22,14]
+print(min(time_list))
+print(max(time_list))
