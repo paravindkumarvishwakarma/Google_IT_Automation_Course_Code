1a28b4e8a76446a5749d79f28295c841e680a613
ParavindVishwakarma1 <pravindkumarvishwakarma@gmail.com>
2024-05-30 22:39:36 +0530

    Recursion Quize

---
+++ Python Crash Course/D21_What_is_recursion.py
@@ -0,0 +1,28 @@
+#What is recursion?
+"""The repeated application of the same procedure to a smaller problem
+   Recursion lets us tackle complex problem by reducing the problem to a similar one
+   In programming, recursion is a way of doing a repetitive task by having a function call itself.
+"""
+
+"""def factorial(n):
+    if n<2:
+        return 1
+    return n * factorial(n-1)"""
+
+def factorial(n):
+  print("Factorial called with " + str(n))
+  if n < 2:
+    print("Returning 1")
+    return 1
+  result = n * factorial(n-1)
+  print("Returning " + str(result) + " for factorial of " + str(n))
+  return result
+
+factorial(4)
+
+
+def recursive_function(parameters):
+    if base_case_condition(parameters):
+        return base_case_value
+    recursive_function(modified_parameters)
+
