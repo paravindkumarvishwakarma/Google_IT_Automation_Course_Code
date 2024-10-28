5b16bf97506223bf398616b3ee228746597609c0
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-10-28 14:25:14 +0000

    Updated new question

--- Python Crash Course/D23_.py
+++ Python Crash Course/D23_.py
@@ -53,7 +53,28 @@ sequence(1, 3)
 # 3, 2, 1
 # 3, 2, 1
 
+#Ques -> 5: Fill in the blanks to complete the “all_numbers” function. This function should return a space-separated string of all #numbers, from the starting   “minimum” variable  up to and including the “maximum” variable that's passed into the function. Complete #the for loop so that a function call like “all_numbers(3,6)” will return the numbers “3 4 5 6”.  
 
+def all_numbers(minimum, maximum):
+  return_string = ""  # Initializes variable as a string
 
+  # Complete the for loop with a range that includes all 
+  # numbers up to and including the "maximum" value.
+  for number in range(minimum, maximum + 1):
+      # Complete the body of the loop by appending the number
+      # followed by a space to the "return_string" variable.
+      return_string += str(number) + " "
 
+  # This .strip command will remove the final " " space 
+  # at the end of the "return_string".
+  return return_string.strip()
+
+# Test cases
+print(all_numbers(2, 6))  # Should be 2 3 4 5 6
+print(all_numbers(3, 10)) # Should be 3 4 5 6 7 8 9 10
+print(all_numbers(-1, 1)) # Should be -1 0 1
+print(all_numbers(0, 5))  # Should be 0 1 2 3 4 5
+print(all_numbers(0, 0))  # Should be 0
+
+Que-6) 
 
