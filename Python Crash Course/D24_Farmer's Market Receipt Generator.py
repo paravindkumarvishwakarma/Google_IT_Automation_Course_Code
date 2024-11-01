883ada3cf073f049e69eac508ab8b29e74e019ae
Paravind Kumar Vishwakarma <pravindkumarvishwakarma@gmail.com>
2024-11-01 17:07:15 +0000

    Basic Problem Solution

--- Python Crash Course/D24_Farmer's Market Receipt Generator.py
+++ Python Crash Course/D24_Farmer's Market Receipt Generator.py
@@ -29,3 +29,14 @@ total = subtotal + tax_amt
 print("Subtotal:", subtotal)
 print("Sales Tax:", tax_amt)
 print("Total:", total)
+
+def file_size(file_info):
+  name, extention, size= file_info
+  return("{:.2f}".format(size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+
+
